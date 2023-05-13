6c73dac89625772a189ee3723c68ffb5d3b16fc0
xfnw <xfnw@riseup.net>
2023-05-13 18:25:16 -0400

    it works!

--- stap.c
+++ stap.c
@@ -2,26 +2,54 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define CLEAR "\033[2J\033[H\033[2J"
+#define usage(name) ( printf("usage: %s height\n", name) )
+
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
 		if (argc != 0)
-			printf("usage: %s height\n", *argv);
+			usage(*argv);
+		return 22;
+	}
+
+	int height = atoi(argv[1]);
+	if (height == 0) {
+		usage(*argv);
 		return 22;
 	}
 
 	while (1) {
-		char delaybuf[5] = "";
-		char cur = 0;
 		int delay = 0;
-		while (delay < 4 && (cur = getchar())) {
-			if (cur == EOF)
-				return 0;
-			if (cur == '\n' || cur == ' ')
-				break;
-			delaybuf[delay++] = cur;
+		char cur = 0;
+
+		{
+			char delaybuf[5] = "";
+
+			while (delay < 4 && (cur = getchar())) {
+				if (cur == EOF)
+					return 0;
+				if (cur == '\n' || cur == ' ')
+					break;
+				delaybuf[delay++] = cur;
+			}
+
+			delay = atoi(delaybuf);
 		}
-		delay = atoi(delaybuf);
-		printf("%d\n", delay);
+
+		printf(CLEAR);
+
+		{
+			int lines = 0;
+			while (lines < height && (cur = getchar())) {
+				if (cur == EOF)
+					return 0;
+				if (cur == '\n')
+					lines++;
+				putchar(cur);
+			}
+		}
+
+		sleep(delay);
 	}
 }
 
